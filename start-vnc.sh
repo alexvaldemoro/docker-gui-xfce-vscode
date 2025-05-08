f9258100a1ee3779545cc69d90e72c5fe01cf3b9
alexvaldemoro <159023223+alexvaldemoro@users.noreply.github.com>
2025-05-08 23:52:59 +0200

    Update start-vnc.sh
    Agregado el cambio de usuario de root a devuser

--- start-vnc.sh
+++ start-vnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Iniciar servidor VNC
-vncserver :1 -geometry 1280x800 -depth 24 && \
+# Iniciar VNC como devuser
+su - devuser -c "vncserver :1 -geometry 1280x800 -depth 24" \
 # Iniciar SSH
 service ssh start && \
 # Mantener el contenedor en ejecución
